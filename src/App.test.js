71660b160ac01ec47bb3ae8bc4a4d7993f8a9db2
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-11-23 02:59:02 -0800

    created tests for button color and text state on click

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, logRoles, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct color initially', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const button = screen.getByRole('button', {name: /Changes to blue/i})
+
+  expect(button).toHaveStyle({backgroundColor: 'red'})
 });
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+  
+  const button = screen.getByRole('button', {name: /Changes to blue/i})
+  fireEvent.click(button)
+
+  expect(button).toHaveStyle({backgroundColor: 'blue'})
+});
+
+test('button turns back to red when clicked after turning blue', () => {
+  const {container} = render(<App />);
+
+  logRoles(container)
+  
+  const button = screen.getByRole('button', {name: /Changes to blue/i})
+  fireEvent.click(button)
+
+  expect(button).toHaveStyle({backgroundColor: 'blue'})
+  expect(button).toHaveTextContent(/Changes to red/i)
+
+  fireEvent.click(button)
+
+  expect(button).toHaveStyle({backgroundColor: 'red'})
+  expect(button).toHaveTextContent(/Changes to blue/i)
+});
+
